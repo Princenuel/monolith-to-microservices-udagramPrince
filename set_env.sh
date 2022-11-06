c5615278c71bed3d6084a5c1f7d4e42a311ac602
Princenuel <emmanuel.chukwuemeka0416@gmail.com>
2022-11-06 19:22:38 +0100

    adding our ci pipeline steps for execution

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=udagram-dbinstance.crvuffzconye.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagramdb4project3
+export AWS_BUCKET=udagramproject3s3bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
